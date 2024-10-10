ef2f798fb9f15699cced14ad9e6c7a5356a80a00
Victor Cheng <victorcheng12345@gmail.com>
2024-10-10 14:04:50 -0700

    Deploy verifying paymaster

--- deploy/1_deploy_verifying_paymaster.ts
+++ deploy/1_deploy_verifying_paymaster.ts
@@ -6,12 +6,27 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const provider = ethers.provider;
   const from = await provider.getSigner().getAddress();
 
-  await hre.deployments.deploy("VerifyingPaymaster", {
+  const result = await hre.deployments.deploy("VerifyingPaymaster", {
     from,
     args: [process.env.ENTRY_POINT_ADDRESS, from],
     log: true,
     deterministicDeployment: true,
   });
+
+  if (result) {
+    if (hre.network.name !== "hardhat" && hre.network.name !== "localhost") {
+      try {
+        await hre.run("verify:verify", {
+          address: result.address,
+          constructorArguments: [process.env.ENTRY_POINT_ADDRESS, from],
+        });
+      } catch (error) {
+        console.error("Error verifying contract:", error);
+      }
+    } else {
+      console.log("Skipping verification on local network");
+    }
+  }
 };
 
 func.skip = async (_: HardhatRuntimeEnvironment): Promise<boolean> => {
